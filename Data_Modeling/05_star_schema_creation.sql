8fd891ee6b1d551825971c759e1e7312ebd5eb3c
Taha Can Tuzunler <can_taha5@hotmail.com>
2026-08-07 18:38:22 +0300

    data modeling starded, first table of star schema: dim_vehicle

---
+++ Data_Modeling/05_star_schema_creation.sql
@@ -0,0 +1,21 @@
+--- Vehicle table has been created that contains vehicle details.
+--- First dimension table of star schema
+
+CREATE TABLE dim_vehicle (
+    vehicle_id SERIAL PRIMARY KEY,
+    year INT,
+    make VARCHAR(50),
+    model VARCHAR(50),
+    trim VARCHAR(50),
+    body VARCHAR(50),
+    transmission VARCHAR(50),
+    color VARCHAR(50),      
+    interior VARCHAR(50)    
+);
+
+--- IMPORTING DATA FROM FIRST TABLE
+--- I USED 'SELECT DISTINCT' TO IMPORT UNIQUE VALUES ONLY
+
+INSERT INTO dim_vehicle (year, make, model, trim, body, transmission, color, interior)
+SELECT DISTINCT year, make, model, trim, body, transmission, color, interior
+FROM first_car_sales;
